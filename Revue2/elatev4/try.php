eecb164c93a87e3e1c5248f94ea0d17953fd8d91
Erwan-J <sti2d3sembat.julvecourt@gmail.com>
2022-05-09 17:29:31 +0200

    setInterval

--- Revue2/elatev4/try.php
+++ Revue2/elatev4/try.php
@@ -5,16 +5,16 @@
 							   // Connexion à la base de données
 								  $idcom = connect_DB();
 							
-							   $inclinaison="SELECT Inclination_panneau FROM MesurePS WHERE id_ps='1' ORDER BY ID DESC ";//selcetionner seulement la consommation
-							   $orientation="SELECT Orientation_panneau FROM MesurePS WHERE id_ps='1' ORDER BY ID DESC";
+							   $inclinaison="SELECT Inclinaison_Panneau FROM MesurePS WHERE id_ps='1' ORDER BY ID DESC ";//selcetionner seulement la consommation
+							   $orientation="SELECT Orientation_Panneau FROM MesurePS WHERE id_ps='1' ORDER BY ID DESC";
 							   $tension="SELECT V_Panneau FROM MesurePS WHERE id_ps='1' ORDER BY ID DESC";
 							   $courant="SELECT I_Panneau FROM MesurePS WHERE id_ps='1' ORDER BY ID DESC";
 							   $temps_marche="SELECT Horodatage FROM MesurePS WHERE id_ps='1' ORDER BY ID DESC";
 								  //////////////////////////////////////////////////////
-								  afficheValeur($inclinaison,$idcom);
-							    afficheValeur($orientation,$idcom);
-								  afficheValeur($courant,$idcom);
-							   afficheValeur($temps_marche,$idcom); 
+								afficheValeur($inclinaison,$idcom);
+							  afficheValeur($orientation,$idcom);
+								afficheValeur($courant,$idcom);
+							  afficheValeur($temps_marche,$idcom); 
 						   //p=U*I il me faut peut être la tension 
 						   //ajouter un parametre d'unité a la fonction ??
 						   header("Refresh:0");
